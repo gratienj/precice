c845c2d9e165d7e702b3f28bdaf2f2e84be5d277
Frédéric Simonis <simonisfrederic@gmail.com>
2024-08-22 15:06:52 +0200

    Add error message for remeshing while subcycling

--- src/precice/impl/ParticipantImpl.cpp
+++ src/precice/impl/ParticipantImpl.cpp
@@ -664,6 +664,7 @@ void ParticipantImpl::resetMesh(
   PRECICE_CHECK(_allowsRemeshing, "Cannot reset meshes. This feature needs to be enabled using <precice-configuration experimental=\"1\" allow-remeshing=\"1\">.");
   PRECICE_TRACE(meshName);
   PRECICE_VALIDATE_MESH_NAME(meshName);
+  PRECICE_CHECK(_couplingScheme->isTimeWindowComplete(), "Cannot remesh while subcycling or iterating. Remeshing is only allowed when the time window is completed.");
   impl::MeshContext &context = _accessor->usedMeshContext(meshName);
 
   PRECICE_DEBUG("Clear mesh positions for mesh \"{}\"", context.mesh->getName());
