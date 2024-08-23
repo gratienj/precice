0691aa1b37f1a766c844f14f8e795a799f5e90bc
Frédéric Simonis <simonisfrederic@gmail.com>
2024-08-23 12:05:56 +0200

    Fix provided partition prepare in serial

--- src/partition/ProvidedPartition.cpp
+++ src/partition/ProvidedPartition.cpp
@@ -203,14 +203,8 @@ void ProvidedPartition::prepare()
     _mesh->setVertexOffsets(std::move(vertexOffsets));
 
   } else {
-
-    PRECICE_WARN("B1");
     // The only rank of the participant contains all vertices
-    PRECICE_WARN("B1.1 {}", _mesh->nVertices());
-    //PRECICE_ASSERT(_mesh->getVertexDistribution().empty());
-    PRECICE_WARN("before lambda");
     _mesh->setVertexDistribution([&] {
-      PRECICE_WARN("in lambda");
       mesh::Mesh::VertexDistribution vertexDistribution;
       for (int i = 0; i < numberOfVertices; i++) {
         vertexDistribution[0].push_back(i);
@@ -219,10 +213,7 @@ void ProvidedPartition::prepare()
       PRECICE_WARN("end lambda");
       return vertexDistribution;
     }());
-    PRECICE_WARN("B2");
-    PRECICE_ASSERT(_mesh->getVertexOffsets().empty());
     _mesh->setVertexOffsets({numberOfVertices});
-    PRECICE_WARN("B3");
     _mesh->setGlobalNumberOfVertices(numberOfVertices);
   }
 
